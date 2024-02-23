5471795fde975f71c46bed687327751f4dad63b8
thoma <thomashamori@gmail.com>
2024-02-23 12:00:42 -0500

    added a println to the 'main' file

--- Main.java
+++ Main.java
@@ -14,5 +14,6 @@ public class Main {
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.println(arr2[1]);
         System.out.println(arr1[1]);
+        System.out.println("made another change here. change 2...");
     }
 }
